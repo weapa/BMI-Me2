4063c64becb3f630e8fab9c793c88a35150b9256
piyada <waengsothon_p@silpakorn.edu>
2020-02-03 16:00:40 +0700

    update1

---
+++ app/src/main/java/th/ac/su/piyada/bmi_me2/BmiActivity.kt
@@ -0,0 +1,12 @@
+package th.ac.su.piyada.bmi_me2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BmiActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmi)
+    }
+}
